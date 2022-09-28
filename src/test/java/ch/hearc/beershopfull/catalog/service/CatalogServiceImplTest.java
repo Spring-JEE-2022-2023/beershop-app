e7c2b39b49a86ffbb06b2d7973b3ce19fcd174e4
Seb <seb@macbook-pro-de-seb.home>
2022-09-28 06:57:06 +0200

    implement services tests

---
+++ src/test/java/ch/hearc/beershopfull/catalog/service/CatalogServiceImplTest.java
@@ -0,0 +1,39 @@
+package ch.hearc.beershopfull.catalog.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CatalogServiceImplTest {
+
+	@Test
+	void testAddBeerToCatalog() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAllBeersFromCatalog() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteBeer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdateBeer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetBeerById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSaveEvaluation() {
+		fail("Not yet implemented");
+	}
+
+}
